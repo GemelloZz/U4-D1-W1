e817588a21380f2c35d6611c10110b1120c64392
Gabriele Lucarelli <l-gabri1998@live.it>
2025-01-07 15:02:57 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 10 ;
+        int y = 20 ;
+        int sum = multiply(x,y) ;
+
+
+        int z = 30 ;
+        String a =  " Gabriele e giovanni che è bellissimo anche se siciliano ";
+        String result = z + a;
+
+
+System.out.print(result);
+    }
+    public static int multiply(int x, int y)
+    {return x*y;}
+}
+
+
+
